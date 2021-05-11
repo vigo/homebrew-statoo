34be270fa40f5b121f204a8ed7c1444b4921eed3
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-05-11 19:11:41 +0300

    Fix missing homepage info in formula

--- statoo.rb
+++ statoo.rb
@@ -1,6 +1,6 @@
 class Statoo < Formula
   desc "`statoo` is a super simple http GET tool for checking site health"
-  homepage ""
+  homepage "https://github.com/vigo/statoo"
   url "https://github.com/vigo/statoo/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "9188474d779832c37d6d67a853d25a0cd25b6c5f8270e20b5b46e533378ee6a4"
   license "MIT"
