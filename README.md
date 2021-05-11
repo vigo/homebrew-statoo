5980d587ceb4eff894a8e666635f00f224bcaf5d
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-05-11 09:43:27 +0300

    Add formula

---
+++ README.md
@@ -0,0 +1,10 @@
+# Brew Tap for `statoo`
+
+Install latest version of [statoo](https://github.com/vigo/statoo)
+
+```bash
+$ brew tap vigo/statoo
+$ brew install statoo
+```
+
+---
---
+++ statoo.rb
@@ -0,0 +1,23 @@
+class Statoo < Formula
+  desc "`statoo` is a super simple http GET tool for checking site health"
+  homepage "https://github.com/vigo/statoo"
+  url "https://github.com/vigo/statoo/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "9188474d779832c37d6d67a853d25a0cd25b6c5f8270e20b5b46e533378ee6a4"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+      ENV["GOPATH"] = buildpath
+
+      bin_path = buildpath/"src/github.com/vigo/statoo"
+      bin_path.install Dir["*"]
+      cd bin_path do
+        system "go", "build", "-o", bin/"statoo", "."
+      end
+    end
+
+  test do
+    assert_match "1.0.1", shell_output("#{bin}/statoo -version 2>&1", 2)
+  end
+end
