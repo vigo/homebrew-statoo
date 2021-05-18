0ab9d9c1d534d3fdf308c088a26205e65b674629
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-05-18 11:22:35 +0300

    statoo 1.1.3 (update)

--- Formula/statoo.rb
+++ Formula/statoo.rb
@@ -1,8 +1,8 @@
 class Statoo < Formula
   desc "`statoo` is a super simple http GET tool for checking site health"
   homepage "https://github.com/vigo/statoo"
-  url "https://github.com/vigo/statoo/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "fa4fcf1d04046cddc18e38188556dbd3e18e28c6d7e14a368c8d34cbb2b6674c"
+  url "https://github.com/vigo/statoo/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "0fff82233498f74ead312c682f863fbfe0c45d5bd4b5d81ae667545585503b66"
   license "MIT"
 
   bottle do
@@ -15,13 +15,14 @@ class Statoo < Formula
 
   def install
     system "go", "build", *std_go_args
+    (bash_completion/"statoo").write `#{bin}/statoo bash-completion`
   end
 
   test do
     require "open3"
     Open3.popen3("#{bin}/statoo", "-version") do |stdin, stdout, _|
       stdin.close
-      assert_equal "1.1.2", stdout.read.strip
+      assert_equal "1.1.3", stdout.read.strip
     end
   end
 end
