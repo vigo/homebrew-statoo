99f9c77572473771d7707e48f64ec682d7482bc7
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-06-10 16:42:17 +0300

    statoo 1.2.1 (update)

--- Formula/statoo.rb
+++ Formula/statoo.rb
@@ -1,8 +1,8 @@
 class Statoo < Formula
   desc "`statoo` is a super simple http GET tool for checking site health"
   homepage "https://github.com/vigo/statoo"
-  url "https://github.com/vigo/statoo/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "f9b6767656eb191441c21422f7bee3ab4af8e7111fab43cb2b4b3cdcdb3daa33"
+  url "https://github.com/vigo/statoo/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "85e1b2fc6e77c4dcbe7f6dcf91c625ae9fdd3a9ecb0264c9c76cb05002402035"
   license "MIT"
 
   bottle do
@@ -22,7 +22,7 @@ class Statoo < Formula
     require "open3"
     Open3.popen3("#{bin}/statoo", "-version") do |stdin, stdout, _|
       stdin.close
-      assert_equal "1.2.0", stdout.read.strip
+      assert_equal "1.2.1", stdout.read.strip
     end
   end
 end
