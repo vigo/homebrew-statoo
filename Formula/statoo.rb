3ffa6d7dafae8d721cadbc86b3d65f529aa5734b
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2022-01-29 13:19:17 +0000

    statoo: update 1.3.1 bottle.

--- Formula/statoo.rb
+++ Formula/statoo.rb
@@ -6,9 +6,9 @@ class Statoo < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/vigo/homebrew-statoo/releases/download/statoo-1.3.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "3aaabf9ec867f18df0cf9986d8221fc9b6336e323f6bd92b46bab9ea418d9553"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7671eee2247796c86b6c6b433adad0198e09e50a431bef44929b38ad0655192a"
+    root_url "https://github.com/vigo/homebrew-statoo/releases/download/statoo-1.3.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "9df7151bdd8b8aeaa93d8395a8cea5a72f473532f960bd4102cdee60c3a152c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "03a115c4971f8426ac2d7f134601bd46f739011ccfb187d04c62600374e3fd05"
   end
 
   depends_on "go" => :build
